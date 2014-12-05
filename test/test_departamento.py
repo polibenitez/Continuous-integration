c2820217b71ca12ae5a9059bbf4fa1e3425bdeda
zadik <zadikmccold@gmail.com>
2014-12-05 12:29:29 +0100

    imports add to tests files

--- test/test_departamento.py
+++ test/test_departamento.py
@@ -4,6 +4,10 @@ from mockito import *
 
 from src.Departamento import *
 
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 
 __author__ = 'usuarioupo'
 
